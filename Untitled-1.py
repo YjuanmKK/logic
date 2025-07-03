81123149fd126aee5c8a8f12c36f1d0c141ba7b4
JUAN PABLO DE OLIVEIRA <06242589044@senacrs.edu.br>
2025-07-03 15:01:36 -0300

    calculadora

--- Untitled-1.py
+++ Untitled-1.py
@@ -2,5 +2,7 @@ a = int(input("Digite um valor para a:"))
 b = int(input("digite um valor para b:"))
 
 soma = a + b
+subt = a - b
 
-print(f"gitO resultado é {soma}")
+print(f"O resultado é {soma}")
+print(f"o resultado da subtração é {subt}")
